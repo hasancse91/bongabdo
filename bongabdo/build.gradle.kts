b04b5ab6a6511b8d302dae810ea825a54bc03623
Hasan <hasan.cse91@gmail.com>
2024-07-25 14:58:27 +0600

    Gradle dependency updated

--- bongabdo/build.gradle.kts
+++ bongabdo/build.gradle.kts
@@ -9,9 +9,7 @@ java {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
+    testImplementation(libs.bundles.junitBundle)
 }
 
 tasks.test {
