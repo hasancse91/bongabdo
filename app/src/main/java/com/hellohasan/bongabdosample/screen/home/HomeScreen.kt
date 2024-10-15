3bf8d20a88504aa98600180958e41ff089005ffe
Hasan <hasan.cse91@gmail.com>
2024-10-15 15:45:49 +0600

    Date picker added in HomeScreen

--- app/src/main/java/com/hellohasan/bongabdosample/screen/home/HomeScreen.kt
+++ app/src/main/java/com/hellohasan/bongabdosample/screen/home/HomeScreen.kt
@@ -1,10 +1,56 @@
 package com.hellohasan.bongabdosample.screen.home
 
+import android.icu.util.Calendar
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 @Composable
 fun HomeScreen() {
-    Text("Home Screen Content")
+    val context = LocalContext.current
+    val calendar = Calendar.getInstance()
+
+    var selectedDate by remember { mutableStateOf(getDateString(calendar)) }
+
+    val datePickerDialog = android.app.DatePickerDialog(
+        context,
+        { _, year, month, dayOfMonth ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, month)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            selectedDate = getDateString(calendar)
+        },
+        calendar.get(Calendar.YEAR),
+        calendar.get(Calendar.MONTH),
+        calendar.get(Calendar.DAY_OF_MONTH)
+    )
+
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text("Select a Gregorian Calendar Date")
+        Button(
+            content = {
+                Text(text = selectedDate)
+            },
+            onClick = { datePickerDialog.show() }
+        )
+    }
+}
+
+fun getDateString(calendar: Calendar): String {
+    val formatter = SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault())
+    return formatter.format(calendar.time)
 }
 
