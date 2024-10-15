7d09e93a7e5a47b9b7b5440199c3630128ff3384
Hasan <hasan.cse91@gmail.com>
2024-10-15 23:26:33 +0600

    Date picker and bongabdo conversion done

---
+++ app/src/main/java/com/hellohasan/bongabdosample/screen/home/BongabdoDateCard.kt
@@ -0,0 +1,40 @@
+package com.hellohasan.bongabdosample.screen.home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BongabdoDateCard(titleResId: Int, bongabdoDate: String) {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Text(
+                text = stringResource(titleResId),
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = bongabdoDate,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
+}
--- app/src/main/java/com/hellohasan/bongabdosample/screen/home/HomeScreen.kt
+++ app/src/main/java/com/hellohasan/bongabdosample/screen/home/HomeScreen.kt
@@ -2,9 +2,10 @@ package com.hellohasan.bongabdosample.screen.home
 
 import android.icu.util.Calendar
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -14,43 +15,41 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import java.text.SimpleDateFormat
-import java.util.Locale
+import com.hellohasan.bongabdosample.R
 
 @Composable
 fun HomeScreen() {
     val context = LocalContext.current
-    val calendar = Calendar.getInstance()
+    var calendarState by remember { mutableStateOf(Calendar.getInstance()) }
 
-    var selectedDate by remember { mutableStateOf(getDateString(calendar)) }
+    var selectedDate by remember { mutableStateOf(getDateString(calendarState)) }
 
     val datePickerDialog = android.app.DatePickerDialog(
         context,
         { _, year, month, dayOfMonth ->
+            val calendar = Calendar.getInstance()
             calendar.set(Calendar.YEAR, year)
             calendar.set(Calendar.MONTH, month)
             calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
 
+            calendarState = calendar
+
             selectedDate = getDateString(calendar)
         },
-        calendar.get(Calendar.YEAR),
-        calendar.get(Calendar.MONTH),
-        calendar.get(Calendar.DAY_OF_MONTH)
+        calendarState.get(Calendar.YEAR),
+        calendarState.get(Calendar.MONTH),
+        calendarState.get(Calendar.DAY_OF_MONTH)
     )
 
     Column(modifier = Modifier.padding(16.dp)) {
         Text("Select a Gregorian Calendar Date")
         Button(
-            content = {
-                Text(text = selectedDate)
-            },
+            content = { Text(text = selectedDate) },
             onClick = { datePickerDialog.show() }
         )
+        Text("Bongabdo Date Output", modifier = Modifier.padding(top = 32.dp, bottom = 16.dp))
+        BongabdoDateCard(R.string.bangla_academy_title, getBanglaAcademyBongabdo(calendarState))
+        Spacer(modifier = Modifier.height(16.dp))
+        BongabdoDateCard(R.string.drik_shiddhanta_title, getDrikShiddhantaBongabdo(calendarState))
     }
 }
-
-fun getDateString(calendar: Calendar): String {
-    val formatter = SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault())
-    return formatter.format(calendar.time)
-}
-
