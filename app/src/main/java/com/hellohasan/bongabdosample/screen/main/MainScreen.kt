567c210639eb53dd591cad34e3a4f159293665f4
Hasan <hasan.cse91@gmail.com>
2024-10-15 15:12:18 +0600

    Topbar added

--- app/src/main/java/com/hellohasan/bongabdosample/screen/main/MainScreen.kt
+++ app/src/main/java/com/hellohasan/bongabdosample/screen/main/MainScreen.kt
@@ -1,7 +1,10 @@
 package com.hellohasan.bongabdosample.screen.main
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
@@ -9,10 +12,14 @@ import androidx.navigation.compose.composable
 import com.hellohasan.bongabdosample.screen.home.HomeScreen
 import com.hellohasan.bongabdosample.screen.settings.SettingsScreen
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
     val navController = androidx.navigation.compose.rememberNavController()
     Scaffold(
+        topBar = {
+            TopAppBar(title = { Text("Bongabdo") })
+        },
         bottomBar = { BottomNavigationBar(navController) }
     ) { innerPadding ->
         NavHost(
--- app/src/main/java/com/hellohasan/bongabdosample/screen/main/Screen.kt
+++ app/src/main/java/com/hellohasan/bongabdosample/screen/main/Screen.kt
@@ -3,6 +3,12 @@ package com.hellohasan.bongabdosample.screen.main
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class Screen(val route: String, val title: String, val icon: ImageVector) {
